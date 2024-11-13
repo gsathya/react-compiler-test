88d042d2ac35fe93175bdcc46d5cbe0af5b12e28
Sathya Gunasekaran <gsathya.ceg@gmail.com>
2024-11-13 23:21:22 +0530

    Enable experimental flag

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
-        reactCompiler: true
+        reactCompiler: {
+            environment: {
+                enableJsxOutlining: true,
+            },
+          },
     }
 };
 
